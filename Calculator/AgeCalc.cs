0a7a71a4caf4b387a7520aba350a29b2c591d988
ThimHeider <thim.heider@gmail.com>
2016-09-13 13:36:00 +0200

    Made entire age calculator

---
+++ Calculator/AgeCalc.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class AgeCalc
+    {
+        public int CalculateAge(DateTime result)
+        {
+            if (result.CompareTo(DateTime.Now) == -1)
+            {
+                TimeSpan difference = DateTime.Now - result;
+                int age = (int)(difference.TotalDays / 365.25);
+                return age;
+            } else
+            {
+                return 0;
+            }
+        }
+    }
+}
--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -10,8 +10,34 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Success!");
-            Console.ReadLine();
+            AgeCalc calcy = new AgeCalc();
+
+            DateTime result;
+            
+            bool successfull = false;
+
+            while (successfull == false)
+            {
+                Console.WriteLine("Please enter your date of birth:");
+                string input = Console.ReadLine();
+                bool success = DateTime.TryParse(input, out result);
+                if (success)
+                {
+                    if (result.CompareTo(DateTime.Now) == -1)
+                    {
+                        Console.WriteLine("Your age is: " + calcy.CalculateAge(result) + ".");
+                        Console.WriteLine("");
+                        Console.WriteLine("Press any key to close this window.");
+                    }
+                    successfull = true;
+                }
+                else
+                {
+                    Console.WriteLine("Your input format is invalid, it should be dd-mm-yyyy.");
+                    Console.WriteLine("");
+                }
+            }
+            Console.Read();
         }
     }
 }
